ef059e1853665bae29e49287433e69c3bcb1c756
Nurlan <nurik.droid@gmail.com>
2019-10-06 23:30:20 +0600

    added service discovery

--- src/main/java/kz/balalar/balalararticleservice/resources/BalalarArticleResource.java
+++ src/main/java/kz/balalar/balalararticleservice/resources/BalalarArticleResource.java
@@ -1,17 +1,19 @@
 package kz.balalar.balalararticleservice.resources;
 
-import kz.balalar.balalararticleservice.models.*;
+import kz.balalar.balalararticleservice.models.ArticleItem;
+import kz.balalar.balalararticleservice.models.Comment;
+import kz.balalar.balalararticleservice.models.PodcastItem;
+import kz.balalar.balalararticleservice.models.UserRating;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
-import org.springframework.web.reactive.function.client.WebClient;
 
-import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
+
+//import org.springframework.web.reactive.function.client.WebClient;
 
 @RestController
 @RequestMapping("/article")
@@ -20,21 +22,16 @@ public class BalalarArticleResource {
     @Autowired
     private RestTemplate restTemplate;
 
-    @Autowired
-    private WebClient.Builder webClientBuilder;
+//    @Autowired
+//    private WebClient.Builder webClientBuilder;
 
     @RequestMapping("/{userId}")
     public List<ArticleItem> getArticle(@PathVariable("userId") String userId) {
-
-
-
-        UserRating trends = restTemplate.getForObject("http://localhost:8084/trends/users/" + userId, UserRating.class);
-
-
+        UserRating trends = restTemplate.getForObject("http://balalar-trends-service/trends/users/" + userId, UserRating.class);
 
         return trends.getUserRating().stream().map(trend -> {
-            Comment comment = restTemplate.getForObject("http://localhost:8082/comments/" + trend.getTrendId(), Comment.class);
-            PodcastItem podcast = restTemplate.getForObject("http://localhost:8083/podcast/" + trend.getTrendId(), PodcastItem.class);
+            Comment comment = restTemplate.getForObject("http://balalar-comments-service/comments/" + trend.getTrendId(), Comment.class);
+            PodcastItem podcast = restTemplate.getForObject("http://balalar-podcasts-service/podcast/" + trend.getTrendId(), PodcastItem.class);
 //            Comment comment = webClientBuilder.build()
 //                    .get()
 //                    .uri("http://localhost:8082/comments/" + trend.getTrendId())
